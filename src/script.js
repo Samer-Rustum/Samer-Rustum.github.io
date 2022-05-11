007870809ec1ebb8ce0bf7eede41abcd63b7bed2
Lamona <samer.rustum@live.com>
2022-05-11 00:07:47 -0400

    update website for 1B

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 ScrollReveal().reveal(".reveal", {
-  interval:200,
-  distance:"20px",
+  interval: 200,
+  distance: "20px",
 });
 
-ScrollReveal().reveal(".delay-1000ms", {
-  distance:"20px",
-  delay:1000,
+ScrollReveal().reveal(".delay-500ms", {
+  distance: "20px",
+  delay: 500,
 });
